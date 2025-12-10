44339cbfc657b84ca9a32261c8ba102adfd0a685
우기 <jungwk1020@gmail.com>
2025-12-10 18:07:50 +0900

    fix: 오타수정

--- src/utils/getImagePath.ts
+++ src/utils/getImagePath.ts
@@ -1,5 +1,5 @@
 const getImagePath = (title: string, tag: string, createdDate: string, updatedDate?: string) => {
-  return `/api/og?title=${encodeURIComponent(title)}&tag=${encodeURIComponent(tag)}&date=${createdDate}&v=${updatedDate ? updatedDate : ""}`;
+  return `/api/og?title=${encodeURIComponent(title)}&tag=${encodeURIComponent(tag)}&date=${createdDate}?v=${updatedDate ? updatedDate : ""}`;
 };
 
 export default getImagePath;
