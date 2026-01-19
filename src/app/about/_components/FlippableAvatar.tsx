6eb3651ebfe9bec4814d41fdbaa7b97baf494a55
우기 <jungwk1020@gmail.com>
2026-01-19 15:14:38 +0900

    fix: 스타일 최적화 수정

--- src/app/about/_components/FlippableAvatar.tsx
+++ src/app/about/_components/FlippableAvatar.tsx
@@ -11,19 +11,19 @@ export default function FlippableAvatar({
 }) {
   return (
     <div className="group size-32 max-sm:hidden perspective-[1000px] cursor-pointer">
-      <div className="relative h-full w-full transition-all duration-1500 transform-3d group-hover:transform-[rotateY(360deg)] will-change-transform">
-        <div className="absolute inset-0 backface-hidden">
+      <div className="relative h-full w-full transition-transform duration-1500 transform-3d group-hover:transform-[rotateY(360deg)] will-change-transform">
+        <div className="absolute inset-0 backface-hidden [-webkit-backface-visibility:hidden]">
           <Avatar className="size-full border-2 border-chart-1 rounded-full p-1">
             <AvatarImage src={frontSrc} alt="아바타 앞면" />
             <AvatarFallback>{fallback}</AvatarFallback>
           </Avatar>
         </div>
 
-        <div className="absolute inset-0 h-full w-full rounded-full border-2 border-chart-2 p-1 bg-white backface-hidden overflow-hidden transform-[rotateY(180deg)] will-change-transform">
+        <div className="absolute inset-0 h-full w-full rounded-full border-2 border-chart-2 p-1 bg-white backface-hidden [-webkit-backface-visibility:hidden] overflow-hidden transform-[rotateY(180deg)]">
           <img
             src={backSrc}
             alt="아바타 뒷면"
-            className="h-full w-full object-contain scale-120 top-2 relative"
+            className="h-full w-full object-contain scale-120 translate-y-1"
           />
         </div>
       </div>
