9ddec96a76ee4ac206adbfabe14d1163355f5b11
우기 <jungwk1020@gmail.com>
2026-02-04 01:01:30 +0900

    feat: 썸네일 디자인 변경에 따른 캐시 초기화

--- src/utils/getImagePath.ts
+++ src/utils/getImagePath.ts
@@ -1,5 +1,5 @@
 const getImagePath = (title: string, tag: string, createdDate: string, updatedDate?: string) => {
-  return `/api/og?title=${encodeURIComponent(title)}&tag=${encodeURIComponent(tag)}&createdDate=${createdDate}&updatedDate=${updatedDate ? updatedDate : ""}`;
+  return `/api/og?title=${encodeURIComponent(title)}&tag=${encodeURIComponent(tag)}&createdDate=${createdDate}&updatedDate=${updatedDate ? updatedDate : ""}&v=1`;
 };
 
 export default getImagePath;
