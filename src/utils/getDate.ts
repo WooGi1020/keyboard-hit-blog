bb2752c6492f7514434f2411a4493e1a8e81650d
우기 <jungwk1020@gmail.com>
2026-02-04 02:49:56 +0900

    fix: formatting utc 파싱 로직으로 수정

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,9 +1,12 @@
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
 
 export function getFormattedDate(dateString: string, format: string = "YYYY. MM. DD") {
-  return dayjs(dateString).format(format);
+  return dayjs.utc(dateString).format(format);
 }
 
 export function parseDate(dateString: string): dayjs.Dayjs {
-  return dayjs(dateString);
+  return dayjs.utc(dateString);
 }
